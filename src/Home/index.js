1ea2a63382ae8d2d6a6009ecbcd024ee547e3e2f
Monica <monica@fortalezatec.com.br>
2022-02-16 22:08:49 -0300

    'Commit'

---
+++ src/Home/index.js
@@ -0,0 +1,10 @@
+import {Container, Name, Description} from './styles'
+
+export default function Home() {
+  return (
+      <Container>
+          <Name>Sujeito programador</Name>
+          <Description>Descrição</Description>
+      </Container>
+  );
+}
